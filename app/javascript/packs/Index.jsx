f38d5373866c3f7c045ea753c3e3868429331744
Katie Daily <kkdaily31@gmail.com>
2021-03-29 22:05:43 -0400

    setup react router routing for entry point of frontend

---
+++ app/javascript/packs/Index.jsx
@@ -0,0 +1,20 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+import React from "react";
+import { render } from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from "../components/App";
+import { BrowserRouter as Router } from "react-router-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>,
+    document.body.appendChild(document.createElement("div"))
+  );
+});
