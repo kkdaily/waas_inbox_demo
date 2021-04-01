e7cd81bc479b4996c9d812ecd111f7c2124c3daf
Katie Daily <kkdaily31@gmail.com>
2021-04-01 16:01:29 -0400

    add loading state when signing in

--- app/javascript/components/SignIn.jsx
+++ app/javascript/components/SignIn.jsx
@@ -6,17 +6,20 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
 
 function SignIn() {
   let history = useHistory();
   let auth = useAuth();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
 
   async function login(ev) {
     ev.preventDefault();
     setError('');
+    setIsLoading(true);
 
     try {
       const resp = await signInUser({ username, password });
@@ -24,10 +27,14 @@ function SignIn() {
       if (resp.data) {
         auth.updateUser(resp.data);
         history.push('/conversations');
-      } else {
+      } 
+      else {
         setError('Invalid username or password.');
       }
-    } catch (err) {
+      setIsLoading(false);
+    } 
+    catch (err) {
+      setIsLoading(false);
       setError('Something went wrong. Please try again.');
     }
   }
@@ -44,9 +51,17 @@ function SignIn() {
           <Form.Control value={password} onChange={(ev) => setPassword(ev.target.value)} type="password" placeholder="Password" />
         </Form.Group>
         
-        <Button className="signin-btn my-3" size="lg" block onClick={login} variant="orange" type="submit">
-          Sign in
-        </Button>
+        {isLoading ? (
+          <Button className="signin-btn my-3" size="lg" block onClick={login} variant="orange" disabled>
+            Signing in...
+            <Spinner className="ml-2" as="span" animation="border" size="md" role="status" aria-hidden="true" />
+          </Button>
+        ) : (
+          <Button className="signin-btn my-3" size="lg" block onClick={login} variant="orange">
+            Sign in
+          </Button>
+        )}
+        
 
         <span>{error}</span>
       </Form>
