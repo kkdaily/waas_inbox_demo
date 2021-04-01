51e49f97f55312141d2ab61859dd829db6350b42
Katie Daily <kkdaily31@gmail.com>
2021-03-31 21:00:01 -0400

    redirect un-authed users to sign in page

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-//import Routes from "../routes/Index";
-
-//export default props => <>{Routes}</>;
-
-// //import './styles/App.scss';
 import {
   Switch,
   Route,
   Redirect,
 } from 'react-router-dom';
 import Inbox from '../containers/Inbox';
+import PrivateRoute from "./PrivateRoute";
+import SignIn from "./SignIn";
 
 function App() {
   return (
     <>
       <Switch>
-        <Route path="/conversations">
-          <Inbox />
+        <Route path="/signin">
+          <SignIn />
         </Route>
+        <PrivateRoute path="/conversations">
+          <Inbox />
+        </PrivateRoute>
         <Route path="*">
           <Redirect to="/conversations" />
         </Route>
