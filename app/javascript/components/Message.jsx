76ad693f0345b18992baa74e15568920148432ab
Katie Daily <kkdaily31@gmail.com>
2021-04-01 01:23:41 -0400

    show either "you" or "to me" when a message is expanded depending on who the logged in user is

--- app/javascript/components/Message.jsx
+++ app/javascript/components/Message.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import Collapse from 'react-bootstrap/Collapse';
-//import '../styles/Message.scss';
+import { useAuth } from '../hooks/useAuth';
 import { getRelativeTimeInWords } from '../utils/time';
 
-function Message({ id, author_profile_image_url, author_first_name, author_last_name, content, sent_at, isCollapsible }) {
+function Message({ id, sender_id, sender_profile_image_url, sender_first_name, sender_last_name, content, sent_at, isCollapsible }) {
+  let { user } = useAuth();
   const [isOpen, setIsOpen] = useState(!isCollapsible);
-  const authorFullName = `${author_first_name} ${author_last_name}`;
+  const senderFullName = `${sender_first_name} ${sender_last_name}`;
+  const messageTo = user.id === sender_id ? "you" : "to me";
 
   // toggles message open state
   function openMessage() {
@@ -15,14 +17,14 @@ function Message({ id, author_profile_image_url, author_first_name, author_last_
   };
 
   function renderAuthorProfileImage() {
-    if (author_profile_image_url) {
-      // render author's profile image if exists
+    if (sender_profile_image_url) {
+      // render sender's profile image if exists
       return (
-        <img className="rounded-circle float-left mr-3" width="55" src={author_profile_image_url} alt="profile" />
+        <img className="rounded-circle float-left mr-3" width="55" src={sender_profile_image_url} alt="profile" />
       )
     } else {
-      // render fallback image using author's initials
-      const initials = author_first_name.charAt(0) + author_last_name.charAt(0);
+      // render fallback image using sender's initials
+      const initials = sender_first_name.charAt(0) + sender_last_name.charAt(0);
       return (
         <div className="fallback-profile-img mr-3 float-left rounded-circle">
           <strong className="initials font-weight-bold">
@@ -40,13 +42,15 @@ function Message({ id, author_profile_image_url, author_first_name, author_last_
         {renderAuthorProfileImage()}
 
         {/* Author and message details */}
-        <div className={"author-header text-decoration-none " + (isCollapsible ? 'show-pointer-on-hover' : '')}>
+        <div className={"sender-header text-decoration-none " + (isCollapsible ? 'show-pointer-on-hover' : '')}>
           <div className="d-flex align-items-center justify-content-between mb-1">
-            <span className="font-weight-bold">{authorFullName}</span>
+            <span className="font-weight-bold">{senderFullName}</span>
             <small>{getRelativeTimeInWords(sent_at)}</small>
           </div>
-          <div className={"text-muted overflow-hidden text-nowrap text-truncate " + (isOpen ? "invisible" : "visible")}>
-            {content}
+          {/* show truncated message as a preview */}
+          <div className={"text-muted overflow-hidden text-nowrap text-truncate "}>
+            {/* show "you" if the logged in user is the sender of the message. Otherwise, show "to me" */}
+            {isOpen ? messageTo : content }
           </div>
         </div>
       </div>
@@ -54,7 +58,7 @@ function Message({ id, author_profile_image_url, author_first_name, author_last_
       {/* Collapsible message content */}
       <Collapse in={isOpen} timeout={0}>
         <div className="message-content mt-1">
-          <span className="pt-4 pb-5 d-inline-block">{content}</span>
+          <span className="pb-5 d-inline-block">{content}</span>
         </div>
       </Collapse>
     </div>
