54854f93d61d153370591ae18a5c2403755921b8
Katie Daily <kkdaily31@gmail.com>
2021-03-31 20:59:11 -0400

    add routes to messages and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+
+      # get 'messages', to: 'messages#index'
+      # get 'messages/users/:user_id', to: 'messages#show'
+      get 'conversations', to: 'conversations#index'
+      get 'conversations/:id', to: 'conversations#show'
+      post 'messages', to: 'messages#create'
+
+      #get "login", to: "sessions#new"
+      post "sessions", to: "sessions#create"
+      get "sessions", to: "sessions#destroy"
+      get "sessions/user", to: "sessions#show"
+    end
+  end
+
+  root 'homepage#index'
+  get '/*path' => 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
