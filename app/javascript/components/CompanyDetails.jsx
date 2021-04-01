358eecd6a95ddbfa29e35dfca6826969200b46fe
Katie Daily <kkdaily31@gmail.com>
2021-03-31 22:06:02 -0400

    remove unused import

--- app/javascript/components/CompanyDetails.jsx
+++ app/javascript/components/CompanyDetails.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
-//import '../styles/CompanyDetails.scss';
 
 function CompanyDetails({ name, logo_url, website_url, batch, size, location, industry }) {
   return (
