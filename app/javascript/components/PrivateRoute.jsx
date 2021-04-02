713bd828346c3d1fc89094ba1e95405995c2534a
Katie Daily <kkdaily31@gmail.com>
2021-04-02 03:49:50 -0400

    check if user is logged in for protected routes before rendering the children components

--- app/javascript/components/PrivateRoute.jsx
+++ app/javascript/components/PrivateRoute.jsx
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect, Route } from 'react-router';
 import { useAuth } from '../hooks/useAuth';
+import { loginStatus } from '../api/auth';
 
 function PrivateRoute({ children, ...rest }) {
   let auth = useAuth();
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        auth.user ? (
-          children
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/signin",
-              state: { from: location }
-            }}
-          />
-        )
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getLoginStatus();
+  }, []);
+
+  async function getLoginStatus() {
+    try {
+      const resp = await loginStatus();
+
+      if (resp.data.logged_in) {
+        auth.update({ isLoggedIn: true, user: resp.data.user });
+      } else {
+        auth.update({ isLoggedIn: false, user: {} });
       }
-    />
+      setIsLoading(false);
+    } catch (err) {
+      console.error('error getting login status:', err);
+    }
+  };
+
+  return (
+    <>
+      {isLoading ? (
+        <div>loading</div>
+      ) : (
+        <Route
+          {...rest}
+          render={({ location }) =>
+            auth.isLoggedIn ? (
+              children
+            ) : (
+              <Redirect
+                to={{
+                  pathname: "/signin",
+                  state: { from: location }
+                }}
+              />
+            )
+          }
+        />
+      )}
+    </>
   );
-}
+};
 
 export default PrivateRoute;
