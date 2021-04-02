410102be4b97e43ac5c1ffac71b7fe3a6566e6fd
Katie Daily <kkdaily31@gmail.com>
2021-04-02 10:25:30 -0400

    remove unused scopes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,4 @@ class User < ApplicationRecord
 
   has_many :messages, class_name: 'Message', :foreign_key => "sender_id", :dependent => :delete_all
   has_many :messages, class_name: 'Message', :foreign_key => "receiver_id", :dependent => :delete_all
-
-  scope :is_candidate, -> (user_id) {
-    Candidate.where(user_id: user_id).exists?
-  }
-
-  scope :is_founder, -> (user_id) {
-    Founder.where('founders.user_id = ?', user_id).exists?
-  }
 end
