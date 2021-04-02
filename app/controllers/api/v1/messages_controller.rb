640091b674ffd4f879b481fbacb97cd4706a69c6
Katie Daily <kkdaily31@gmail.com>
2021-04-02 06:40:32 -0400

    add comments explaining SQL queries and normalize the column names being selected as

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -15,14 +15,15 @@ class Api::V1::MessagesController < ApplicationController
     search = "%#{params[:search]}%"
     offset = params[:offset]
 
-    # if the logged in user is a candidate, get all messages where the candidate has sent/received it
-    # grouped by company 
     if is_candidate(user_id)
 
+      # get all conversations where the candidate has either sent/received the message
+      # ordered by when the most recent message in the conversation was sent at,
+      # grouped by founder
       @conversations = Message.find_by_sql(["
         SELECT 
-        LEFT(m.content, 30) as clipped_message, comp.logo_url as company_logo_url, 
-            comp.name as company_name, m.created_at as last_message_sent_at, f.user_id as id
+        LEFT(m.content, 30) as clipped_message, comp.logo_url as profile_image_url, 
+            comp.name as name, m.created_at as last_message_sent_at, f.user_id as id
         FROM messages as m
 
         INNER JOIN founders AS f 
@@ -40,7 +41,7 @@ class Api::V1::MessagesController < ApplicationController
           ON comp.id = f.company_id
           WHERE (m.receiver_id = :user_id OR m.sender_id = :user_id)
           AND (comp.name ILIKE :search OR m.content ILIKE :search)
-          GROUP BY comp.id
+          GROUP BY f.user_id
           LIMIT 10
           OFFSET :offset
         ) 
@@ -51,41 +52,49 @@ class Api::V1::MessagesController < ApplicationController
         :offset => offset
       }])
 
-    # if the logged in user is a founder, get all messages where the founder has sent/received it
-    # grouped by candidate
-    elsif is_founder(user_id)
-
+    else
+      # get all conversations where the founder has either sent/received the message
+      # ordered by when the most recent message in the conversation was sent at,
+      # grouped by candidate
       @conversations = Message.find_by_sql(["
-        SELECT * FROM 
+        SELECT 
+        LEFT(m.content, 30) as clipped_message, u.profile_image_url, CONCAT(u.first_name, ' ', u.last_name) as name, 
+        m.created_at as last_message_sent_at, can.user_id as id
+        FROM messages as m
+
+        INNER JOIN founders AS f 
+        ON (f.user_id = m.sender_id OR f.user_id = m.receiver_id)
+        INNER JOIN companies as comp 
+        ON comp.id = f.company_id
+        INNER JOIN candidates AS can
+        ON (can.user_id = m.sender_id OR can.user_id = m.receiver_id)
+        INNER JOIN users AS u
+        ON can.user_id = u.id
+
+        WHERE m.created_at IN
         (
-          SELECT DISTINCT ON (can.user_id)
-          LEFT(m.content, 30) as clipped_message, u.profile_image_url as company_logo_url, 
-            CONCAT(u.first_name, ' ', u.last_name) as company_name, m.created_at as last_message_sent_at, can.user_id as id
+          SELECT MAX(m.created_at)
           FROM messages AS m
           INNER JOIN candidates AS can
           ON (can.user_id = m.sender_id OR can.user_id = m.receiver_id)
-          INNER JOIN founders as f
-          ON f.user_id = :user_id
           INNER JOIN users AS u
           ON can.user_id = u.id
+          INNER JOIN founders AS f 
+          ON (f.user_id = m.sender_id OR f.user_id = m.receiver_id)
           INNER JOIN companies as comp 
           ON comp.id = f.company_id
           WHERE (m.receiver_id = :user_id OR m.sender_id = :user_id)
           AND (u.first_name ILIKE :search OR u.last_name ILIKE :search OR m.content ILIKE :search)
-          ORDER BY can.user_id
+          GROUP BY can.user_id
           LIMIT 10
           OFFSET :offset
         ) 
-        AS conversations 
-        ORDER BY conversations.last_message_sent_at DESC
+        ORDER BY m.created_at DESC
       ", {
         :user_id => user_id,
         :search => search,
         :offset => offset
       }])
-
-    else 
-      puts "invalid user role"
     end
 
     render json: @conversations
@@ -97,7 +106,8 @@ class Api::V1::MessagesController < ApplicationController
     founder_id = params[:id]
     interlocutor_id = params[:id]
 
-    # get all messages between 2 users
+    # get all messages between a candidate and a founder for a conversation
+    # and the founder's company's details
     @conversation = Message.find_by_sql(["
       SELECT conversation.messages_data, conversation.company_data FROM (
         SELECT 
--- app/javascript/components/ConversationCard.jsx
+++ app/javascript/components/ConversationCard.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import { getRelativeTimeInWords } from '../utils/time';
 
-function ConversationCard({ id, company_logo_url, company_name, last_message_sent_at, clipped_message }) {
+function ConversationCard({ id, profile_image_url, name, last_message_sent_at, clipped_message }) {
   return (
     <Link key={id} to={`/conversations/${id}`} className="ConversationCard text-decoration-none">
       <Card className="inbox-card pl-3">
-        <Card.Img variant="top" src={company_logo_url} />
+        <Card.Img variant="top" src={profile_image_url} />
         <Card.Body>
           <div className="header">
-            <strong className="company-name mb-1">{company_name}</strong>
+            <strong className="company-name mb-1">{name}</strong>
             <small className="text-muted">{getRelativeTimeInWords(last_message_sent_at)}</small>
           </div>
           <Card.Text className="text-muted">
