5088de26d7c6e8bd4e5efe9112961f252303c4b6
Katie Daily <kkdaily31@gmail.com>
2021-04-05 06:55:27 -0400

    delete migrations since indexes are already present on founders and candidates

--- db/migrate/20210405090110_add_indexes_to_founders.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexesToFounders < ActiveRecord::Migration[6.1]
-  def change
-    add_index :founders, :user_id
-    add_index :founders, :company_id
-  end
-end
--- db/migrate/20210405090844_add_index_to_candidates.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToCandidates < ActiveRecord::Migration[6.1]
-  def change
-    add_index :candidates, :user_id
-  end
-end
