8b444a350861df1ef3967e9c2e8f6072a9f6f7e3
Katie Daily <kkdaily31@gmail.com>
2021-04-04 23:05:43 -0400

    add scopes for Message model and set maximum content length to 1000

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,13 @@ class Message < ApplicationRecord
   belongs_to :user, class_name: 'User', :foreign_key => "sender_id"
   belongs_to :user, class_name: 'User', :foreign_key => "receiver_id"
 
-  validates :content, presence: true, length: { minimum: 1 }
+  validates :content, presence: true, length: { minimum: 1, maximum: 1000 }
   validates :receiver_id, presence: true
   validates :sender_id, presence: true
+
+  scope :sent_by_user, -> (user_id) { where(sender_id: user_id) }
+  scope :received_by_user, -> (user_id) { where(receiver_id: user_id) }
+  scope :filter_by_content, -> (query) { where("content ILIKE ?", "%#{query}%") }
+  scope :filter_by_company_name, -> (query) { where("companies.name ILIKE ?", "%#{query}%") }
+  scope :most_recent, -> { select("MAX(messages.created_at)") }
 end
