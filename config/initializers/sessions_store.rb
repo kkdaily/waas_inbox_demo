3132d6a28e7783887b053b61790ebacd0c75dd86
Katie Daily <kkdaily31@gmail.com>
2021-04-05 00:27:39 -0400

    remove domain option

--- config/initializers/sessions_store.rb
+++ config/initializers/sessions_store.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: '_waas-inbox-demo', domain: :all
+  Rails.application.config.session_store :cookie_store, key: '_waas-inbox-demo'
 else
   Rails.application.config.session_store :cookie_store, key: '_waas-inbox-demo'
 end
