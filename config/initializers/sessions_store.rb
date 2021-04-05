71a726dcb4c57e55be72fbd05f4d3aa60f553873
Katie Daily <kkdaily31@gmail.com>
2021-04-05 00:20:05 -0400

    set domain all on prod

--- config/initializers/sessions_store.rb
+++ config/initializers/sessions_store.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: '_waas-inbox-demo', domain: ENV['DOMAIN_URL']
+  Rails.application.config.session_store :cookie_store, key: '_waas-inbox-demo', domain: :all
 else
   Rails.application.config.session_store :cookie_store, key: '_waas-inbox-demo'
 end
