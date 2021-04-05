e8e594f97f1ba68ca60a8d7226ccdd13c7faf823
Katie Daily <kkdaily31@gmail.com>
2021-04-05 03:07:12 -0400

    fix message preview not truncating properly

--- app/javascript/components/ConversationCard.jsx
+++ app/javascript/components/ConversationCard.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Card from 'react-bootstrap/Card';
 import { getRelativeTimeInWords } from '../utils/time';
 
 function ConversationCard({ id, profile_image_url, name, last_message_sent_at, clipped_message }) {
   return (
     <Link key={id} to={`/conversations/${id}`} className="ConversationCard text-decoration-none">
-      <Card className="inbox-card pl-3">
-        <Card.Img variant="top" src={profile_image_url} />
-        <Card.Body>
-          <div className="header">
-            <strong className="company-name mb-1">{name}</strong>
+      <div className="content-wrapper py-3">
+        
+        {/* Profile image */}
+        <img className="rounded float-left mr-3" width="70" src={profile_image_url} alt="profile" />
+
+        {/* Author and message details */}
+        <div className="sender-header text-decoration-none show-pointer-on-hover">
+          <div className="d-flex align-items-center justify-content-between mb-1">
+            <span className="name font-weight-bold">{name}</span>
             <small className="text-muted">{getRelativeTimeInWords(last_message_sent_at)}</small>
           </div>
-          <Card.Text className="text-muted">
+          {/* show truncated message as a preview */}
+          <div className={"text-muted overflow-hidden text-nowrap text-truncate"}>
             {clipped_message}
-          </Card.Text>
-        </Card.Body>
-      </Card>
+          </div>
+        </div>
+      </div>
     </Link>
   )
 };
