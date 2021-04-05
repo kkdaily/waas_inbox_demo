96f745153e303c9c9eb8df5b4279aa9a670b53e1
Katie Daily <kkdaily31@gmail.com>
2021-04-04 23:13:12 -0400

    add additional validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ApplicationRecord
+  before_save { username.downcase! }
   has_secure_password
-  validates :username, presence: true, uniqueness: true
-  validates :password, presence: true
 
-  has_many :candidates
-  has_many :founders
+  has_many :candidates, dependent: :destroy
+  has_many :founders, dependent: :destroy
+  has_many :messages, class_name: 'Message', :foreign_key => "sender_id", dependent: :destroy
+  has_many :messages, class_name: 'Message', :foreign_key => "receiver_id", dependent: :destroy
 
-  has_many :messages, class_name: 'Message', :foreign_key => "sender_id", :dependent => :delete_all
-  has_many :messages, class_name: 'Message', :foreign_key => "receiver_id", :dependent => :delete_all
+  VALID_USERNAME_REGEX = /[\w]+/
+  VALID_NAME_REGEX = /[A-Za-z]+/
+
+  validates :username, presence: true, uniqueness: true, length: { minimum: 1, maximum: 10 }, format: { with: VALID_USERNAME_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :first_name, presence: true, length: { minimum: 1, maximum: 20 }, format: { with: VALID_NAME_REGEX }
+  validates :last_name, presence: true, length: { minimum: 1, maximum: 20 }, format: { with: VALID_NAME_REGEX }
 end
