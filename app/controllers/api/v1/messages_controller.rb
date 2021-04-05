05493bd9b474eeaef380239f3de99734ace42684
Katie Daily <kkdaily31@gmail.com>
2021-04-05 04:41:53 -0400

    refactor more ActiveRecord queries into Message scopes

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -7,61 +7,29 @@ class Api::V1::MessagesController < ApplicationController
     search = params[:search]
     offset = params[:offset]
     is_candidate = User.is_candidate?(user_id)
-     
-    # get all messages where the current user has sent or received
-    tables = Message.joins("
-      INNER JOIN founders 
-      ON (founders.user_id = messages.sender_id OR founders.user_id = messages.receiver_id)
-    ")
-    .joins("
-      INNER JOIN companies 
-      ON companies.id = founders.company_id
-    ")
-    .joins("
-      INNER JOIN candidates
-      ON (candidates.user_id = messages.sender_id OR candidates.user_id = messages.receiver_id)
-    ")
-    .joins("
-      INNER JOIN users
-      ON candidates.user_id = users.id
-    ")
-  
-    # get all messages where the current user was either the sender or receiver
-    user_messages = tables
-      .sent_by_user(user_id)
-      .or(tables.received_by_user(user_id))
 
-    # filter messages by query text
-    filtered_messages = user_messages
+    messages = Message.messages_sent_or_received_by_user(user_id)
+
+    filtered_messages = messages
       .filter_by_company_name(search)
-      .or(user_messages.filter_by_content(search))
-      .or(user_messages.filter_by_user_name(search))
+      .or(messages.filter_by_content(search))
+      .or(messages.filter_by_user_name(search))
 
     if is_candidate
-      # group most recent messages by founder
       grouped_messages = filtered_messages.most_recent.group('founders.user_id')
     else
-      # group most recent messages by candidate
       grouped_messages = filtered_messages.most_recent.group('candidates.user_id')
     end
 
-    latest_messages = tables
-      .where(created_at: grouped_messages.pluck('MAX(messages.created_at)'))
-      .order(last_message_sent_at: :desc)
-
-    paginated_messages = latest_messages.limit(10).offset(offset)
+    latest_messages = messages
+      .order_by_latest_message(grouped_messages)
+      .limit(10)
+      .offset(offset)
     
     if is_candidate
-      @conversations = paginated_messages.select("
-        LEFT(messages.content, 130) as clipped_message, companies.logo_url as profile_image_url, 
-        companies.name as name, messages.created_at as last_message_sent_at, founders.user_id as id
-      ")
+      @conversations = latest_messages.candidate_conversation_info
     else
-      @conversations = paginated_messages.select("
-        LEFT(messages.content, 130) AS clipped_message, users.profile_image_url, 
-        CONCAT(users.first_name, ' ', users.last_name) AS name, 
-        messages.created_at AS last_message_sent_at, candidates.user_id AS id
-      ")
+      @conversations = latest_messages.founder_conversation_info
     end
 
     render json: @conversations
@@ -73,33 +41,10 @@ class Api::V1::MessagesController < ApplicationController
     founder_id = params[:id]
     interlocutor_id = params[:id]
 
-    # sanitize 
-    @interlocutor_id = ActiveRecord::Base.connection.quote(interlocutor_id)
-    @user_id = ActiveRecord::Base.connection.quote(user_id)
-
-    tables = Message
-      .joins("INNER JOIN users ON (users.id = messages.sender_id)")
-      .joins("INNER JOIN founders ON (founders.user_id = #{@interlocutor_id} OR founders.user_id = #{@user_id})")
-      .joins("INNER JOIN candidates ON (candidates.user_id = #{@interlocutor_id} OR candidates.user_id = #{@user_id})")
-      .joins("INNER JOIN companies ON companies.id = founders.company_id")
-
-    messages = tables
-      .where(sender_id: user_id, receiver_id: interlocutor_id)
-      .or(tables.where(sender_id: interlocutor_id, receiver_id: user_id))
-
-    message_thread = messages.select("
-      messages.id as id, messages.sender_id as sender_id, messages.content as content,
-      users.first_name as sender_first_name, users.last_name as sender_last_name,
-      users.profile_image_url as sender_profile_image_url, messages.created_at as sent_at
-    ").order(:created_at)
-
-    company = messages.select("
-      companies.id, name, batch, website_url, logo_url, size, industry, location
-    ").first
-
-    candidate = messages.select("
-      candidates.id, first_name, last_name, profile_image_url, candidates.status
-    ").first
+    conversation = Message.conversation_between_users(user_id, interlocutor_id)
+    message_thread = conversation.message_thread_info.order(:created_at)
+    company = conversation.company_info.first
+    candidate = conversation.candidate_info.first
 
     @conversations = {
       :messages_data => message_thread,
--- app/models/message.rb
+++ app/models/message.rb
@@ -7,9 +7,67 @@ class Message < ApplicationRecord
   validates :sender_id, presence: true
 
   scope :sent_by_user, -> (user_id) { where(sender_id: user_id) }
+
   scope :received_by_user, -> (user_id) { where(receiver_id: user_id) }
+
   scope :filter_by_content, -> (query) { where("content ILIKE ?", "%#{query}%") }
+
   scope :filter_by_company_name, -> (query) { where("companies.name ILIKE ?", "%#{query}%") }
+
   scope :filter_by_user_name, -> (query) { where("first_name ILIKE ?", query).or(where("last_name ILIKE ?", query)) }
+
   scope :most_recent, -> { select("MAX(messages.created_at)") }
+
+  scope :candidate_conversation_info, -> { 
+    select("
+      LEFT(messages.content, 130) as clipped_message, companies.logo_url as profile_image_url, 
+      companies.name as name, messages.created_at as last_message_sent_at, founders.user_id as id
+    ") 
+  }
+  scope :founder_conversation_info, -> {
+    select("
+      LEFT(messages.content, 130) AS clipped_message, users.profile_image_url, 
+      CONCAT(users.first_name, ' ', users.last_name) AS name, 
+      messages.created_at AS last_message_sent_at, candidates.user_id AS id
+    ")
+  }
+
+  scope :candidate_info, -> { select("candidates.id, first_name, last_name, profile_image_url, candidates.status") }
+
+  scope :company_info, -> { select("companies.id, name, batch, website_url, logo_url, size, industry, location") }
+
+  scope :message_thread_info, -> { 
+    select("
+      messages.id as id, messages.sender_id as sender_id, messages.content as content,
+      users.first_name as sender_first_name, users.last_name as sender_last_name,
+      users.profile_image_url as sender_profile_image_url, messages.created_at as sent_at
+    ")
+  }
+
+  scope :messages_sent_or_received_by_user, -> (user_id) { 
+    joins("INNER JOIN founders ON (founders.user_id = messages.sender_id OR founders.user_id = messages.receiver_id)")
+    .joins("INNER JOIN companies ON companies.id = founders.company_id")
+    .joins("INNER JOIN candidates ON (candidates.user_id = messages.sender_id OR candidates.user_id = messages.receiver_id)")
+    .joins("INNER JOIN users ON candidates.user_id = users.id")
+    .sent_by_user(user_id)
+    .or(Message.received_by_user(user_id))
+  }
+
+  scope :order_by_latest_message, -> (messages) {
+    where(created_at: messages.pluck('MAX(messages.created_at)'))
+    .order(last_message_sent_at: :desc)
+  }
+
+  scope :conversation_between_users, -> (user_id, interlocutor_id) {
+    # sanitize 
+    @interlocutor_id = ActiveRecord::Base.connection.quote(interlocutor_id)
+    @user_id = ActiveRecord::Base.connection.quote(user_id)
+
+    joins("INNER JOIN users ON (users.id = messages.sender_id)")
+    .joins("INNER JOIN founders ON (founders.user_id = #{@interlocutor_id} OR founders.user_id = #{@user_id})")
+    .joins("INNER JOIN candidates ON (candidates.user_id = #{@interlocutor_id} OR candidates.user_id = #{@user_id})")
+    .joins("INNER JOIN companies ON companies.id = founders.company_id")
+    .where(sender_id: user_id, receiver_id: interlocutor_id)
+    .or(Message.where(sender_id: interlocutor_id, receiver_id: user_id))
+  }
 end
